357908337c4b74c8bbd9dbac8942f3ea0e32fd78
Irinaabieva <irinaabieva03@gmail.com>
2022-12-07 10:20:22 +0300

    Задание 2

--- src/Human.java
+++ src/Human.java
@@ -2,15 +2,17 @@ public class Human {
     private final int yearOfBirth;
     private final String name;
     private final String town;
+    private  String jobTitle;
 
-    public Human(int yearOfBirth, String name, String town) {
+    public Human(int yearOfBirth, String name, String town, String jobTitle) {
         this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.town = town;
+        this.jobTitle = jobTitle;
     }
 
     @Override
     public String toString() {
-        return "Привет! Меня зовут " + name + " . Я из города " + town + " . Я родился в " + yearOfBirth + " году. Будем знакомы!" ;
+        return "Привет! Меня зовут " + name + " . Я из города " + town + " . Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!" ;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human(1988, "Максим", "Минск");
+        Human maksim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
         System.out.println(maksim.toString());
-        Human anna = new Human(1993, "Аня", "Москва");
+        Human anna = new Human(1993, "Аня", "Москва", "методист образовательных программ");
         System.out.println(anna.toString());
-        Human katya = new Human(1992, "Катя", "Калининград");
-        System.out.println(anna.toString());
-        Human artem = new Human(1995, "Артем", "Москва");
+        Human katya = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
+        System.out.println(katya.toString());
+        Human artem = new Human(1995, "Артем", "Москва","директор по развитию бизнеса");
         System.out.println(artem.toString());
     }
 }
